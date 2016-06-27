6c7153bd741a532c181e9fa874792716913d2292
Tiago Natel de Moura <tiago.natel@neoway.com.br>
2016-06-27 14:20:17 -0300

    Remove unneeded code of jump

--- source/Player.hx
+++ source/Player.hx
@@ -25,25 +25,13 @@ class Player extends FlxSprite
 {
 	public static inline var RUN_SPEED:Int = 90;
 	public static inline var GRAVITY:Int = 440;
-	public static inline var JUMP_SPEED:Int = 250;
-	public static inline var JUMPS_ALLOWED:Int = 2;
+	public static inline var JUMP_SPEED:Int = 500;
 	public static inline var BULLET_SPEED:Int = 200;
 	public static inline var GUN_DELAY:Float = 0.4;
 
 
 	private var _parent:PlayState;
-
-	private var _jumpTime:Float = -1;
-	private var _timesJumped:Int = 0;
-	private var _jumpKeys:Array<FlxKey> = [C, SPACE];
-
-	private var _xgridleft:Int = 0;
-	private var _xgridright:Int = 0;
-	private var _ygrid:Int = 0;
 	private var _state:State;
-
-	public var climbing:Bool = false;
-	private var _onLadder:Bool = false;
 
 	public function new(X:Int, Y:Int, state:State)
 	{
@@ -96,16 +84,6 @@ class Player extends FlxSprite
 		// Reset to 0 when no button is pushed
 		acceleration.x = 0;
 
-		if (climbing)
-		{
-			// Stop falling if you're climbing a ladder
-			acceleration.y = 0;
-		}
-		else
-		{
-			acceleration.y = GRAVITY;
-		}
-
 		if (FlxG.keys.anyPressed([LEFT]))
 		{
 			acceleration.x = -drag.x;
@@ -116,14 +94,6 @@ class Player extends FlxSprite
 		}
 
 		jump(elapsed);
-
-		// Can only climb when not jumping
-		if (_jumpTime < 0)
-		{
-			climb();
-		}
-
-		// Shooting
 
 		// Animations
 		if (velocity.x > 0) {
@@ -166,80 +136,18 @@ class Player extends FlxSprite
 			this.y = 450;
 		}
 
-		// Convert pixel positions to grid positions. Std.int and floor are functionally the same,
-		_xgridleft = Std.int((x + 3) / 16);
-		_xgridright = Std.int((x + width - 3) / 16);
-		// but I hear int is faster so let's go with that.
-		_ygrid = Std.int((y + height - 1) / 16);
-
-
-
-		if (isTouching(FlxObject.FLOOR) && !FlxG.keys.anyPressed(_jumpKeys))
-		{
-			_jumpTime = -1;
-			// Reset the double jump flag
-			_timesJumped = 0;
-		}
-
 		super.update(elapsed);
-	}
-
-	private function climb():Void
-	{
-		if (FlxG.keys.anyPressed([UP, W]))
-		{
-			if (_onLadder)
-			{
-				climbing = true;
-				_timesJumped = 0;
-			}
-		}
-		else if (FlxG.keys.anyPressed([DOWN, S]))
-		{
-			if (_onLadder)
-			{
-				climbing = true;
-				_timesJumped = 0;
-			}
-
-			if (climbing)
-			{
-				velocity.y = RUN_SPEED;
-			}
-		}
 	}
 
 	private function jump(elapsed:Float):Void
 	{
-		if (FlxG.keys.anyJustPressed(_jumpKeys))
+		if (FlxG.keys.anyJustPressed([SPACE]))
 		{
-			if ((velocity.y == 0) || (_timesJumped < JUMPS_ALLOWED)) // Only allow two jumps
-			{
+			if ((velocity.y == 0)) {
 				//FlxG.sound.play("assets/sounds/jump" + Reg.SoundExtension, 1, false);
-				_timesJumped++;
-				_jumpTime = 0;
-				_onLadder = false;
-			}
-		}
-
-		// You can also use space or any other key you want
-		if ((FlxG.keys.anyPressed(_jumpKeys)) && (_jumpTime >= 0))
-		{
-			climbing = false;
-			_jumpTime += elapsed;
-
-			// You can't jump for more than 0.25 seconds
-			if (_jumpTime > 0.25)
-			{
-				_jumpTime = -1;
-			}
-			else if (_jumpTime > 0)
-			{
 				velocity.y = - 0.6 * maxVelocity.y;
 			}
 		}
-		else
-			_jumpTime = -1.0;
 	}
 
 	override public function kill():Void
